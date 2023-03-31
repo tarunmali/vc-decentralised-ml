7a3c9b74b4dd7fae2c1e84f497e9a2fd8021232d
Tarun Mali <tarunmalibackup2@gmail.com>
2023-04-01 01:13:56 +0530

    fb

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -6,7 +6,7 @@ import {stime} from './components/Sstopwatchs';
 
 const SocketContext = createContext();
 // //q
-const socket = io('http://localhost:5000');
+const socket = io();
 // const socket = io('https://seashell-app-55chy.ondigitalocean.app/');
 
 const ContextProvider = ({ children }) => {
